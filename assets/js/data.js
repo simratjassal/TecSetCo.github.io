314fbe5f5689b3ea6bd9488f32432d49a94e7595
Rajinderpal Singh <60106112+sairish2001@users.noreply.github.com>
2022-02-08 15:49:36 +0530

    added data file and community partners folder

---
+++ assets/js/data.js
@@ -0,0 +1,53 @@
+var themesdata = [
+  {
+    name: "Health Care",
+    description: `It is now your turn to contribute to the fast-paced world of medical technology! Be it making
+    applications that are able to better collect and organize information to help medical
+    professionals or applications to help patients in selecting the doctors that will best be able to
+    treat them based on their symptoms or an AI/ML-based solution for problems plaguing the
+    medical world. The possibilities are endless and we look forward to seeing what you guys come
+    up with!`,
+  },
+
+  {
+    name: "FinTech",
+    description: `Financial technology is the technology and innovation that aims to compete with traditional
+    financial methods in the delivery of financial services. Now it's your turn to Innovate something
+    that is much better than conventional means. Run yourselves to build a product that is not been
+    implemented yet.`,
+  },
+
+  {
+    name: "Task Automation",
+    description: `The goal here is to take a task that would generally require a lot of human intervention to be
+    performed and try to automate. The automation can be something as simple as reading a user’s
+    information and filling a form for them or creating a complex chatbot that can adapt, to better
+    help the user. As long as your idea has the potential to have an impact on the system, it’s good
+    to go!`,
+  },
+
+  {
+    name: "Social Issues",
+    description: `The premise here is simple, take any problem that you see around you in your everyday life and
+    try to implement clever solutions for it! Whether it is related to solving theft issues in societies,
+    finding parking spaces in crowded places, or improving attendance systems in schools.`,
+  },
+
+  {
+    name: "Open Innovation",
+    description: `It is time to get your brain juices flowing! This track is one where you can express the full extent
+    of your creativity. Anything from game-dev to an android or web app, Augmented Reality, or any
+    other field that interests you. There are no limits to your creativity and we look forward to seeing
+    your extraordinary creations!`,    
+  },
+];
+
+
+var communitydata=[
+    // {"image":"assets/images/communitypartners/"},
+    {"image":"assets/images/communitypartners/GDSC Jaypee Institute of Information Technology - Sec 128 horizontal color.png"},
+    {"image":"assets/images/communitypartners/GDSC Deogiri Institute of Engineering & Management Studies horizontal color.png"},
+    {"image":"assets/images/communitypartners/GDSC Model Institute of Engineering and Technology horizontal color.png"},
+    {"image":"assets/images/communitypartners/GDSC Bharati Vidyapeeth College of Engineering horizontal color.png"},
+    {"image":"assets/images/communitypartners/GDSC DAV Institute of Engineering and Technology horizontal color.png"},
+]
